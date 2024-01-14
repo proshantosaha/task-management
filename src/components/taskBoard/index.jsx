bcc05557b841ce03195b42f13274b5e7641e2073
proshantosaha <proshantoll71@gmail.com>
2024-01-14 16:38:53 +0600

    create state and one default data for task lit and map the tasklist

--- src/components/taskBoard/index.jsx
+++ src/components/taskBoard/index.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchTask from "./searceTack";
 import TaskActions from "./taskAction";
 import TaskList from "./taskList";
 
 const TaskManeger = () => {
+  const defaultTask = {
+    id: "crypto.randonUUID()",
+    title: "Integration API",
+    description:
+      "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+    tags: ["web", "react", "js"],
+    priority: "high",
+    isFavourite: true,
+  };
+
+  const [tasks, setTasks] = useState([defaultTask]);
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -13,7 +24,7 @@ const TaskManeger = () => {
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions />
 
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
