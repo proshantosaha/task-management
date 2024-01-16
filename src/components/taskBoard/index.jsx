e6c052c16858a30b691fae518eae175c3e41d199
proshantosaha <proshantoll71@gmail.com>
2024-01-16 23:37:30 +0600

    add task feature implement comlite

--- src/components/taskBoard/index.jsx
+++ src/components/taskBoard/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import SearchTask from "./searceTack";
 import TaskActions from "./taskAction";
 import TaskList from "./taskList";
+import AddTaskMdal from "./addTaskModal";
 
 const TaskManeger = () => {
   const defaultTask = {
@@ -14,19 +15,27 @@ const TaskManeger = () => {
     isFavourite: true,
   };
 
-  const handleAddTask = () => {
-    console.log("adding a task");
+  const handleAddTask = (newTask) => {
+    console.log("adding a task", newTask);
+    setTasks([...tasks, newTask]);
+    setShowAddModal(false);
   };
 
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskMdal onSave={handleAddTask} />}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions handleAddTask={handleAddTask} />
+          <TaskActions
+            handleAddTask={() => {
+              setShowAddModal(true);
+            }}
+          />
 
           <TaskList tasks={tasks} />
         </div>
